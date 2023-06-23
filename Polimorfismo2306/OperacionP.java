f1c9846206509aaa660b9d8e2b39edfb7074d66d
Nferraro23 <nico8ferraro@hotmail.com>
2023-06-23 20:42:26 -0300

    Update OperacionP.java

--- Polimorfismo2306/OperacionP.java
+++ Polimorfismo2306/OperacionP.java
@@ -2,21 +2,38 @@
 package Polimorfismo;
 
 import java.util.Scanner;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 
 public abstract class OperacionP {
     
+    ImageIcon intro = new ImageIcon("src/imagen/intro.png");
+    ImageIcon icon = new ImageIcon("src/imagen/intro2.png");
+    
     protected int valor1,valor2,resultado;
     Scanner leer = new Scanner(System.in);
     
     public void PedriDatos(){
-        JOptionPane.showInputDialog(null,"Dame el primer valor :");
-        valor1 = leer.nextInt();
-        
-        JOptionPane.showInputDialog(null,"Dame el segundo valor :");
-        valor2 = leer.nextInt();
-    }
+        String v1 = (String) JOptionPane.showInputDialog(null, 
+                    "Ingrese el primer valor",
+                    "CALCULADORA",
+                    JOptionPane.QUESTION_MESSAGE,
+                    icon,
+                    null,
+                    "");
+      
+             String v2 = (String) JOptionPane.showInputDialog(null, 
+                    "Ingrese el segundo valor",
+                    "CALCULADORA",
+                    JOptionPane.QUESTION_MESSAGE,
+                    icon,
+                    null,
+                    "");
+             
+             valor1 = Integer.parseInt(v1);
+             valor2 = Integer.parseInt(v2);
+       }
     
     public abstract void operaciones ();
     
@@ -25,3 +42,4 @@ public abstract class OperacionP {
      }
     
 }
+
